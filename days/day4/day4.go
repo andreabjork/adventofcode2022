be295e71dc7e65b13444d93f9743b022f4282206
Andrea Bjoernsdottir <andreabjork43@gmail.com>
2022-12-05 08:37:14 +0100

    day 4 and wip day 5

--- days/day4/day4.go
+++ days/day4/day4.go
@@ -1,14 +1,58 @@
 package day4
 
 import (
+	"adventofcode/m/v2/util"
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 func Day4(inputFile string, part int) {
 	if part == 0 {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Completely contained: %d\n", solveA(inputFile, incCompleteOverlap))
 	} else {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Partially contained: %d\n", solveA(inputFile, incPartialOverlap))
 	}
 }
 
+func solveA(inputFile string, inc func(int, int, int, int) int) int {
+	
+	ls := util.LineScanner(inputFile)
+	line, ok := util.Read(ls) 
+	
+	re1 := regexp.MustCompile(`(\d+)-(\d+),(\d+)-(\d+)`)
+	var a,b,c,d, sum int
+	sum = 0
+	for ok {
+		res := re1.FindStringSubmatch(line)
+
+		a, _ = strconv.Atoi(res[1])
+		b, _ = strconv.Atoi(res[2])
+		c, _ = strconv.Atoi(res[3])
+		d, _ = strconv.Atoi(res[4])
+
+		// a-b contained in c-d if a >= c, b <= d
+		// c-d contained in a-b if b >= a, d <= b 
+		sum += inc(a,b,c,d)
+		
+
+		line, ok = util.Read(ls)
+	}
+	return sum
+}
+
+func incCompleteOverlap(a int, b int, c int, d int) int {
+	if ( a >= c && b <= d ) || ( c >= a && d <= b ) {
+		return 1
+	}
+	return 0
+}
+
+func incPartialOverlap(a int, b int, c int, d int) int {
+	// a-b, c-d partially overlap if
+	// a <= c <= b, a <= d <= b, c <= a <= d, c <= b <= d
+	if ( a <= c && c <= b ) || ( a <= d && d <= b) || ( c <= a && a <= d) || ( c <= b && b <= d) {
+		return 1 
+	}
+	return 0
+}
--- days/day5/day5.go
+++ days/day5/day5.go
@@ -1,14 +1,75 @@
 package day5
 
 import (
+	"adventofcode/m/v2/util"
 	"fmt"
+	"regexp"
 )
 
 func Day5(inputFile string, part int) {
 	if part == 0 {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Top Crates: %s\n", solve(inputFile))
 	} else {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Not implmenented.")
 	}
 }
 
+func solve(inputFile string) string {
+
+	ls := util.LineScanner(inputFile)
+	
+	stacks := []*Stack{}
+	var numCrate int
+	line, _ := util.Read(ls)
+	nextLine, ok := util.Read(ls)
+
+	firstLine := true
+	for ok {
+		if firstLine {
+			stacks = append(stacks, &Stack{[]rune{}})
+		}
+
+		crateLabels := []rune(line)
+		// 4 runes per create [ d ] _, we process 1, 5, 9, ...
+		numCrate = 0
+		for i := 1; i < len(crateLabels); i += 4 {
+			if crateLabels[i] != ' ' {
+				stacks[numCrate].push(crateLabels[i])
+			}
+			numCrate++
+		}
+
+		firstLine = false
+		line = nextLine
+		nextLine, ok = util.Read(ls)
+		if nextLine == "" {
+			break	
+		}
+	}
+
+	re := regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`)
+	for ok {
+		res := re1.FindStringSubmatch(line)
+		toMove := strconv
+
+		line, ok = util.Read(ls)
+	}
+}
+
+type Stack struct { 
+	crates []rune 
+}
+
+func (s *Stack) push(r rune) {
+	s.crates = append(s.crates, r)
+}
+
+func (s *Stack) pop() rune {
+	r := s.pop()
+	s.crates = s.crates[:len(s-crates)-1]
+	return r
+}
+
+func (s *Stack) head() rune {
+	return s.crates[len(s.crates)-1]
+}
