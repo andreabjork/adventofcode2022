892d8bdeced4d0f7a3f4ec91d1493edb5e3fe093
Andrea Björnsdóttir <andrea.bjoernsdottir@hexagon.com>
2022-12-11 10:19:25 +0100

    day 11 pt 1

--- days/day10/day10.go
+++ days/day10/day10.go
@@ -1,14 +1,79 @@
 package day10
 
 import (
+	"adventofcode/m/v2/util"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func Day10(inputFile string, part int) {
+	cpu := run(inputFile)
 	if part == 0 {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Sum signals: %d\n", cpu.regSum([]int{20,60,100,140,180,220}) )
 	} else {
-		fmt.Println("Not implmenented.")
+		fmt.Println("Screen image: ")
+		cpu.image()
 	}
+}
+
+func run(inputFile string) *CPU {
+	ls := util.LineScanner(inputFile)
+
+	cpu := &CPU{[]int{1}}
+	line, ok := util.Read(ls)
+	for ok {
+		parts := strings.Split(line, " ")
+		op := parts[0]
+		if op == "noop" {
+			cpu.noop()
+		} else {
+			x, _ := strconv.Atoi(parts[1])
+			cpu.addx(x)
+		}
+		line, ok = util.Read(ls)
+	}
+	return cpu
+}
+
+type CPU struct {
+	hist 	[]int
+}
+
+func (c *CPU) image() {
+	for cyc := 1; cyc < len(c.hist); cyc++ {
+		// Draw # if register-1 <= cyc <= register+1
+		if c.reg(cyc)-1 <= (cyc-1)%40 && c.reg(cyc)+1 >= (cyc-1)%40 {
+			fmt.Printf("#")
+		} else if cyc %40 == 0 {
+			fmt.Println()
+		} else {
+			fmt.Printf(".")
+		}
+	}
+}
+
+func (c *CPU) regSum(cycles []int) int {
+	sum := 0
+	for _, cyc := range cycles {
+		sum += cyc*c.reg(cyc)
+	}
+	return sum
+}
+
+func (c *CPU) reg(cycle int) int {
+	// Returning value during cycle, not after
+	return c.hist[cycle-1]
+}
+
+func (c *CPU) noop() {
+	c.hist = append(c.hist, c.hist[len(c.hist)-1])
+}
+
+func (c *CPU) addx(x int) {
+	for i := 0; i < 1 ; i++{
+		c.noop()
+	}
+	c.hist = append(c.hist, c.hist[len(c.hist)-1]+x)
 }
 
--- days/day11/day11.go
+++ days/day11/day11.go
@@ -2,13 +2,149 @@ package day11
 
 import (
 	"fmt"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func Day11(inputFile string, part int) {
+	monkeys := makeMonkeys(inputFile)
 	if part == 0 {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Level of monkey business 20 rounds: %d\n", MonkeyBusiness(monkeys, 20))
 	} else {
-		fmt.Println("Not implmenented.")
+		fmt.Printf("Level of monkey business 10k rounds: %d\n", MonkeyBusiness(monkeys, 10000))
 	}
 }
 
+func MonkeyBusiness(monkeys []*Monkey, rounds int) int {
+	inspections := make([]int, len(monkeys))
+
+	//showRound(monkeys)
+	for r := 0; r < rounds; r++{
+		for m := 0; m < len(monkeys); m++ {
+			inspections[m] += len(monkeys[m].items)
+			inspectItems(m, monkeys)
+		}
+		//showRound(monkeys)
+	}
+
+	sort.Ints(inspections)
+	return inspections[len(inspections)-1]*inspections[len(inspections)-2]
+}
+
+func showRound(monkeys []*Monkey) {
+	for i := 0; i < len(monkeys); i++{
+		fmt.Printf("Monkey %d: %+v\n", i, monkeys[i].items)
+	}
+}
+
+type Monkey struct {
+	items			[]int
+	op 				func(int) int
+	modOp 			func(int) int
+}
+
+func inspectItems(n int, monkeys []*Monkey) {
+	m := monkeys[n]
+	var throwTo int
+	hasItem, worry := m.pop()
+	for hasItem {
+		worry = m.op(worry)/3
+		throwTo = m.modOp(worry)
+		monkeys[throwTo].push(worry)
+
+		hasItem, worry = m.pop()
+	}
+}
+
+func (m *Monkey) push(x int) {
+	m.items = append(m.items, x)
+}
+
+func (m *Monkey) pop() (bool, int) {
+	if len(m.items) == 0 {
+		return false, 0
+	}
+	head := m.items[0]
+	m.items = m.items[1:]
+	return true, head
+}
+
+func makeMonkeys(inputFile string) []*Monkey {
+	txt, _ := os.ReadFile(inputFile)
+	re := regexp.MustCompile(
+`Monkey (\d+):
+  Starting items: ([\d+|,|\s]*)
+  Operation: new = old (\+|-|\*|\/) ([A-Za-z0-9]*)
+  Test: divisible by (\d+)
+    If true: throw to monkey (\d+)
+    If false: throw to monkey (\d+)`)
+
+	matches := re.FindAllStringSubmatch(string(txt), 10)
+
+	monkeys := []*Monkey{}
+	var itemWorries []string
+	var operator, arg string
+	for _, m := range matches {
+		// Make starting items
+		itemWorries = strings.Split(m[2], ", ")
+		items := make([]int, len(itemWorries))
+		for i := 0; i < len(items); i++ {
+			items[i], _ = strconv.Atoi(itemWorries[i])
+		}
+
+		// Set monkey operation
+		operator = m[3]
+		arg = m[4]
+		var op func(int) int
+		var y int
+		if arg == "old" {
+			switch operator {
+			case "+":
+				op = func(x int) int { return x+x }
+			case "-":
+				op = func(x int) int { return x-x }
+			case "*":
+				op = func(x int) int { return x*x }
+			case "/":
+				op = func(x int) int { return x/x }
+			}
+		} else {
+			y, _ = strconv.Atoi(arg)
+			switch operator {
+			case "+":
+				op = func(x int) int { return x+y }
+			case "-":
+				op = func(x int) int { return x-y }
+			case "*":
+				op = func(x int) int { return x*y }
+			case "/":
+				op = func(x int) int { return x/y }
+			}
+		}
+
+		// Set monkey modulo test
+		test, _ := strconv.Atoi(m[5])
+		iftrue, _ := strconv.Atoi(m[6])
+		iffalse, _ := strconv.Atoi(m[7])
+		modOp := func(x int) int {
+			if x % test == 0 {
+				return iftrue
+			} else {
+				return iffalse
+			}
+		}
+
+		monkeys = append(monkeys,
+			&Monkey{
+				items,
+				op,
+				modOp,
+				},
+			)
+	}
+
+	return monkeys
+}
