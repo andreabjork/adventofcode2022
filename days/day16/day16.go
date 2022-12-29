f8c2fc88d809e7ae54e83b8c85cf413d3d7f68ca
Andrea Bjoernsdottir <andreabjork43@gmail.com>
2022-12-29 17:45:11 +0100

    day 16 wip

--- days/day16/day16.go
+++ days/day16/day16.go
@@ -19,29 +19,43 @@ func Day16(inputFile string, part int) {
 func solve(inputFile string) int {
 	flow := parse(inputFile)
 	// Compute distances from every node to every other node
+	fmt.Printf("Initial path: %+v\n", flow.path)
 	for i, _ := range flow.path {
+		fmt.Printf("%d: %s\n", i, flow.path[i].name)
 		flow.dijkstra(i)
 	}
-
 	for v, val := range flow.dist {
 		for w, d := range val {
 			fmt.Printf("dist %s to %s: %d\n", v.name, w.name, d)
 		}
 	}
 
-	max := 0
-	//max := flow.compute()
-	//for v := 0; v < len(flow.valves); v++ {
-	//	for w := v+1; w < len(flow.valves); w++ {
-	//		alt := flow
-	//		alt.path = flow.alternate(v, w)
-	//		if f := alt.compute(); f > max {
-	//			flow = alt
-	//			max = f
-	//		}
-	//	}
-	//}
+	//max := 0
+	fmt.Println("Before: ", len(flow.path))
+	flow.path = flow.initialize()
+	max := flow.compute()
+	fmt.Println("Initial flow is ", max)
+	
+	for midx := 1; midx < len(flow.path)-1; midx++ {
+		for v := midx; v < len(flow.path); v++ {
+			for w := v+1; w < len(flow.path); w++ {
+				// TODO this needs to be more robust
+				alt := flow
+				alt.path = flow.alternate(v, w)
+				if f := alt.compute(); f > max {
+					flow = alt
+					max = f
+				}
+				fmt.Println("-------")
+			}
+		}
+	}
 
+	fmt.Println("BEST FLOW IS THIS ONE")
+	for i := 0; i < len(flow.path); i++ {
+		fmt.Printf("%s -> ", flow.path[i].name)
+	}
+	fmt.Println(max)
 	return max
 }
 
@@ -68,26 +82,61 @@ func (f *Flow) alternate(v, w int) []*Valve {
 	return path
 }
 
-func (f *Flow) compute() int {
-	f.time = 0
-	for i := 0; i < len(f.path); i++ {
-		f.open(f.path[i-1], f.path[i])
+func (f *Flow) initialize() []*Valve {
+	path := []*Valve{f.path[0]}
+	for i := 1; i < len(f.path); i++ {
+		if f.path[i].rate > 0 {
+			path = append(path, f.path[i])
+		}
 	}
-	f.timePasses(30 - f.time)
+	return path
+}
 
+func (f *Flow) compute() int {
+	f.flow = 0
+	f.time = 0
+	f.active = []*Valve{}
+	if f.path[0].rate > 0 {
+		f.open(f.valves["AA"], f.path[0])
+	}
+	for i := 0; i < len(f.path)-1; i++ {
+		if f.path[i+1].rate > 0 {
+			f.open(f.path[i], f.path[i+1])
+		}
+	}
+	fmt.Printf("All open, now passing %d time\n", 31-f.time)
+	n := 30-f.time
+	for i := 0; i < n; i++ {
+		f.timePasses(1)
+	}
+	fmt.Printf("Computed flow: t=%d, f=%d\n", f.time, f.flow)
 	return f.flow
 }
 
 func (f *Flow) open(from *Valve, to *Valve) {
-	t := f.dist[from][to]
-	f.timePasses(t + 1)
+	t, ok := f.dist[from][to]
+	if !ok{
+		fmt.Println("FATAL")
+	} else {
+		fmt.Printf("Heading %s -> %s ( %d minutes )\n", from.name, to.name, t+1)
+	}
+	for i := 0; i < t+1; i++ {
+		f.timePasses(1)
+	}
 	f.active = append(f.active, to)
+	fmt.Printf("Opened %s; t=%d, f: %d\n", to.name, f.time, f.flow)
 }
 
 func (f *Flow) timePasses(seconds int) {
+	f.time += seconds
+	fmt.Printf("t=%d | Valves ", f.time)
+	press := 0
 	for _, ov := range f.active {
+		fmt.Printf("%s, ", ov.name)
 		f.flow += seconds * ov.rate
+		press += seconds * ov.rate
 	}
+	fmt.Printf("are open, releasing %d pressure\n", press)
 }
 
 type Valve struct {
@@ -149,43 +198,41 @@ func (f *Flow) dijkstra(start int) {
 	if f.dist[f.path[start]] == nil {
 		f.dist[f.path[start]] = map[*Valve]int{}
 		for _, t := range f.path[start].tunnels {
-			fmt.Printf("here setting f.dist[%s][%s]\n", f.path[start].name, t.name)
 			f.dist[f.path[start]][t] = 1
 		}
 		f.dist[f.path[start]][f.path[start]] = 0
 	}
 
-	unvisited := f.path[start+1:]
+	unvisited := copy(f.path[start+1:])
 	unvisited = append(unvisited, f.path[:start]...)
 	// Iterate until all nodes have been visited
 	i := 0
-	fmt.Printf("Unvisited: %d\n", len(unvisited))
 	for len(unvisited) > 0 {
 		// Check all neighbours of unvisited node
 		for k := 0; k < len(unvisited[i].tunnels); k++ {
 			// If node is reachable, visit it
 			if dist, ok := f.dist[f.path[start]][unvisited[i].tunnels[k]]; ok {
-				fmt.Printf("visiting %s\n", unvisited[i].name)
-				f.dist[f.path[start]][unvisited[i]] = dist + 1
-				fmt.Println("neighbours")
+				if initDist, ok := f.dist[f.path[start]][unvisited[i]]; ok && initDist > dist+1 {
+					// distance was already set in the initialization step
+						f.dist[f.path[start]][unvisited[i]] = dist + 1
+				} else if !ok {
+						f.dist[f.path[start]][unvisited[i]] = dist + 1
+				}
 				f.updateNeighbours(f.path[start], unvisited[i])
 				// remove element
 				arr := unvisited[:i]
 				unvisited = append(arr, unvisited[i+1:]...)
-				
 				if len(unvisited) == 0 {
 					break
 				} else {
-					fmt.Println(".i", i)
-					fmt.Println(".len", len(unvisited))
 					i = i % len(unvisited)
-
 				}
 			}
 		}
-
-		fmt.Println("i", i)
-		fmt.Println("len", len(unvisited))
+		i++
+		if i >= len(unvisited) && len(unvisited) > 0 {
+			i = i % len(unvisited)
+		}
 	}
 }
 
@@ -196,4 +243,12 @@ func (f *Flow) updateNeighbours(start, v *Valve) {
 			f.updateNeighbours(start, v.tunnels[k])
 		}
 	}
+}
+
+func copy(path []*Valve) []*Valve {
+	npath := []*Valve{}
+	for i := 0; i < len(path); i++ {
+		npath = append(npath, path[i])
+	}
+	return npath
 }
